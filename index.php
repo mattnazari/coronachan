77c52571ffabd56829d62d44466e8f3f29ed5771
matthewnazari <matthew.nazari8@gmail.com>
2020-02-23 04:18:43 -0800

    sections finished

--- index.php
+++ index.php
@@ -32,10 +32,10 @@
 
         <div class='spacer'></div>
         <section class='causes sections'>
-            <div class='causesCont'>
-                <div class='causesImg'></div>
+            <div class='sectionCont'>
+                <div class='causesImg sectionImg'></div>
                 <div class='leftTxtCont'>
-                    <p class='sectionTitle'>causes</p>
+                    <p class='sectionTitle'>CAUSES</p>
                     <h2 class='sectionHeading'>How does<br/>it spread?</h2>
                     <ul class='list'>
                     <div class='listRow'>
@@ -61,12 +61,67 @@
         <!---------------------------- Prevention Section !----------------------------------->
 
         <div class='spacer'></div>
-        <section class='prevention sections'></section>
+        <section class='prevention sections'>
+        <div class='sectionCont sectionContRight'>
+                <div class='rightTxtCont'>
+                    <p class='sectionTitle'>PREVENTION</p>
+                    <h2 class='sectionHeading'>Stay clean,<br/>be safe.</h2>
+                    <ul class='list'>
+                    <div class='listRow'>
+                    <img src='./assets/graphics/num1.svg' class='nums'>
+                    <span class='bold'>Avoid</span>
+                    <li class='listTxt'>close contact with sick individuals.</li>
+                    </div>
+                    <div class='listRow'>
+                    <img src='./assets/graphics/num2.svg' class='nums'>
+                    <span class='bold'>Avoid</span>
+                    <li class='listTxt'>touching your eyes, nose, and mouth.</li>
+                    </div>
+                    <div class='listRow'>
+                    <img src='./assets/graphics/num3.svg' class='nums'>
+                    <span class='bold'>Stay </span>
+                    <li class='listTxt'>home when you are sick.</li>
+                    </div>
+                    <div class='listRow'>
+                    <img src='./assets/graphics/num4.svg' class='nums'>
+                    <span class='bold'>Wash </span>
+                    <li class='listTxt'>your hands regularly.</li>
+                    </div>
+                </ul>
+                </div>
+                <div class='preventionImg sectionImg'></div>
+            </div>
+        </section>
 
         <!---------------------------- Symptoms Section !----------------------------------->
         
         <div class='spacer'></div>
-        <section class='symptoms sections'></section>
+        <section class='symptoms sections'>
+        <div class='sectionCont'>
+                <div class='symptomsImg sectionImg'></div>
+                <div class='leftTxtCont'>
+                    <p class='sectionTitle'>SYMPTOMS</p>
+                    <h2 class='sectionHeading'>How to know<br/>if you have it.</h2>
+                    <ul class='list'>
+                    <div class='listRow'>
+                    <img src='./assets/graphics/num1.svg' class='nums'>
+                    <span class='bold'>Fever</span>
+                    <li class='listTxt'>and cold-like symptoms.</li>
+                    </div>
+                    <div class='listRow'>
+                    <img src='./assets/graphics/num2.svg' class='nums'>
+                    <span class='bold'>Cough,</span>
+                    <li class='listTxt'>typically a dry type.</li>
+                    </div>
+                    <div class='listRow'>
+                    <img src='./assets/graphics/num3.svg' class='nums'>
+                    <span class='bold'>Shortness</span>
+                    <li class='listTxt'>of breath.</li>
+                    </div>
+                </ul>
+                </div>
+            </div>
+        </section>
 
         <!---------------------------- Awareness Section !----------------------------------->
 
