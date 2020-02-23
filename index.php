a605d403bcccf14b65434fd9edbd4123f54adb61
matthewnazari <matthew.nazari8@gmail.com>
2020-02-23 03:33:40 -0800

    section setup / organization

--- index.php
+++ index.php
@@ -9,8 +9,11 @@
     </head>
     <body>
         <div class='wrapper'>
+
+        <!---------------------------- Header !----------------------------------->
+
         <header class='header'>
-        <h1 class=logo>Corona-chan</h1>
+        <h1 class=logo><a href='#'>Corona-chan</a></h1>
         <ul class='headerItemContainer'>
         <li class='headerItems'><a href='#'>Causes</a></li>
         <li class='headerItems'><a href='#'>Prevention</a></li>
@@ -19,13 +22,61 @@
         <li class='headerItems'><a href='#'>Sign In</a></li>
         </ul>
         </header>
+
+        <!---------------------------- Post Section !----------------------------------->
+
         <div class='spacer'></div>
-        <section class='section1 sections'></section>
-        <section class='section2 sections'></section>
-        <section class='section3 sections'></section>
-        <section class='section4 sections'></section>
-        <section class='section5 sections'></section>
-        <section class='section6 sections'></section>
+        <section class='postSection sections'></section>
+
+        <!---------------------------- Causes Section !----------------------------------->
+
+        <div class='spacer'></div>
+        <section class='causes sections'>
+            <div class='causesCont'>
+                <div class='causesImg'></div>
+                <div class='leftTxtCont'>
+                    <p class='sectionTitle'>causes</p>
+                    <h2 class='sectionHeading'>How does<br/>it spread?</h2>
+                    <ul class='list'>
+                    <div class='listRow'>
+                    <img src='./assets/graphics/num1.svg' class='nums'>
+                    <span class='bold'>Direct</span>
+                    <li class='listTxt'>person-to-person contact.</li>
+                    </div>
+                    <div class='listRow'>
+                    <img src='./assets/graphics/num2.svg' class='nums'>
+                    <span class='bold'>Contact</span>
+                    <li class='listTxt'>with infected surfaces or objects.</li>
+                    </div>
+                    <div class='listRow'>
+                    <img src='./assets/graphics/num3.svg' class='nums'>
+                    <span class='bold'>Respiratory </span>
+                    <li class='listTxt'>droplets from sneezing or coughing.</li>
+                    </div>
+                </ul>
+                </div>
+            </div>
+        </section>
+
+        <!---------------------------- Prevention Section !----------------------------------->
+
+        <div class='spacer'></div>
+        <section class='prevention sections'></section>
+
+        <!---------------------------- Symptoms Section !----------------------------------->
+        
+        <div class='spacer'></div>
+        <section class='symptoms sections'></section>
+
+        <!---------------------------- Awareness Section !----------------------------------->
+
+        <div class='spacer'></div>
+        <section class='awareness sections'></section>
+
+        <!---------------------------- Footer Section !----------------------------------->
+
+        <div class='spacer'></div>
+        <section class='footer sections'></section>
         </div>
     </body>
     <script src="app.js" async defer></script>
