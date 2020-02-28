6d5f9c5739f50a7208b6281e964fcb7a4e75e757
micahmones <micahmones@gmail.com>
2020-02-28 02:30:06 -0800

    sanitize post and delete queries, minor css fixes

--- create_post.php
+++ create_post.php
@@ -15,8 +15,27 @@ $post_title = $_POST['post-title'];
 $post_image = $_POST['image-url'];
 $post_content = $_POST['post-content'];
 
-$query = createPost($username, $post_title, $post_content, $post_image);
-$post = runQuery($query);
+// search for line breaks
+if (strpos($post_content, "\n") !== FALSE) {
+  // new line 2 <br /> php function
+  $new_str = nl2br($post_content);
+}
 
-header("Content-type: application/json");
-echo json_encode($post);
+$mysqli = new mysqli("localhost", "root", "root", "coronachan_db");
+if ($mysqli->connect_error) {
+  exit('Error connecting to database'); //Should be a message a typical user could understand in production
+}
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$mysqli->set_charset("utf8mb4");
+
+$stmt = $mysqli->prepare("INSERT INTO posts (username, post_title, post_content, post_image) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $username, $post_title, $new_str, $post_image);
+$stmt->execute();
+if ($stmt->affected_rows === 0) exit('No rows updated');
+$stmt->close();
+
+// $query = createPost($username, $post_title, $post_content, $post_image);
+// $post = runQuery($query);
+
+// header("Content-type: application/json");
+// echo json_encode($post);
